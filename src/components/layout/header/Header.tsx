18a4ea2045d0eb61e4af12836e4c3c0f67ce27d6
Jimmy Lenister <103216309+JimmyLenister@users.noreply.github.com>
2026-03-05 16:36:28 +0530

    changes committed for the final nav bar.

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React from "react";
 import {
   Search,
   Eye,
@@ -11,303 +13,270 @@ import {
 import "./Header.css";
 
 const Header: React.FC = () => {
-  const [showMegaMenu, setShowMegaMenu] = useState(false);
-  const [showEyeglasses, setShowEyeglasses] = useState(false);
-  const [showContact, setShowContact] = useState(false);
-  const [showAccessories, setShowAccessories] = useState(false);
-  const [showBrands, setShowBrands] = useState(false);
-  const [eyeTest, setEyeTest] = useState(false);
-
   return (
     <>
-      {/* Top Navbar */}
-      <div className="top-navbar">
-        <div className="top-left">
-          <Headphones size={16} /> Need Help?{" "}
-          <span className="phone">Call 1800-266-0123</span>
-        </div>
+     {/* TOP NAVBAR */}
+      <div className="top-bar bg-dark text-white py-1">
+        <div className="container-fluid">
 
-        {/* Search */}
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search for Eyeglasses, Sunglasses"
-            className="search-input"
-          />
-          <Search className="search-icon" size={18} />
-        </div>
+          <div className="d-flex justify-content-between align-items-center">
 
-        <div className="top-right">
-          <span>
-            <li
-              className="nav-item-top"
-              onMouseEnter={() => setEyeTest(true)}
-              onMouseLeave={() => setEyeTest(false)}
-            >
-              <Eye size={16} /> Eye Testing ▾
+            {/* LEFT */}
+            <div className="d-flex align-items-center gap-2">
+              <Headphones size={14} />
+              <span className="help-text">
+                Need Help? <span className="text-info fw-bold">1800-266-0123</span>
+              </span>
+            </div>
 
-              {eyeTest && (
-                <div className="dropdown-top">
-                  <ul>
-                    <li>Digital Eye Strain Test</li>
-                    <li>Online Eye Screening</li>
-                    <li>Instore Free Eye Test</li>
-                    <li>Book Appointment</li>
+            {/* SEARCH (Hidden on small screens) */}
+            <div className="search-box d-none d-md-flex align-items-center bg-white rounded px-2">
+              <Search size={14} className="text-muted me-2" />
+              <input
+                type="text"
+                className="form-control border-0 shadow-none"
+                placeholder="Search for Eyeglasses, Sunglasses"
+              />
+            </div>
+
+            {/* RIGHT MENU */}
+            <div className="d-flex align-items-center gap-3">
+
+              {/* Desktop Menu */}
+              <div className="top-links d-none d-md-flex align-items-center gap-3">
+
+                <div className="dropdown hover-dropdown">
+                  <span className="d-flex align-items-center gap-1">
+                    <Eye size={14}/> Eye Testing ▾
+                  </span>
+
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Digital Eye Strain Test</a></li>
+                    <li><a className="dropdown-item" href="#">Online Eye Screening</a></li>
+                    <li><a className="dropdown-item" href="#">Instore Free Eye Test</a></li>
+                    <li><a className="dropdown-item" href="#">Book Appointment</a></li>
                   </ul>
                 </div>
-              )}
-            </li>
-          </span>
 
-          <span>Hearing Test</span>
-          <span>
-            <User size={16} /> Sign In
-          </span>
-          <span>
-            <Truck size={16} /> Track Order
-          </span>
-          <span>
-            <MapPin size={16} /> Find A Store
-          </span>
+                <span>Hearing Test</span>
+
+                <span className="d-flex align-items-center gap-1">
+                  <User size={14}/> Sign In
+                </span>
+
+                <span className="d-flex align-items-center gap-1">
+                  <Truck size={14}/> Track Order
+                </span>
+
+                <span className="d-flex align-items-center gap-1">
+                  <MapPin size={14}/> Find Store
+                </span>
+
+              </div>
+
+              {/* MOBILE MENU */}
+              <div className="dropdown d-md-none">
+
+                <button
+                  className="btn btn-sm text-white"
+                  data-bs-toggle="dropdown"
+                >
+                  Menu
+                </button>
+
+                <ul className="dropdown-menu dropdown-menu-end">
+
+                  <li><a className="dropdown-item">Eye Testing</a></li>
+                  <li><a className="dropdown-item">Hearing Test</a></li>
+                  <li><a className="dropdown-item">Sign In</a></li>
+                  <li><a className="dropdown-item">Track Order</a></li>
+                  <li><a className="dropdown-item">Find Store</a></li>
+
+                </ul>
+
+              </div>
+
+            </div>
+
+          </div>
         </div>
       </div>
 
-      {/* Main Navbar */}
-      <div className="main-navbar">
-        <div className="logo">
-          TITAN <span>EYE+</span>
-        </div>
+      {/* MAIN NAVBAR */}
+      <nav className="navbar navbar-expand-lg bg-white border-bottom">
+        <div className="container-fluid">
 
-        <ul className="menu">
-          {/* Eyeglasses */}
-          <li
-            onMouseEnter={() => setShowEyeglasses(true)}
-            onMouseLeave={() => setShowEyeglasses(false)}
+          <a className="navbar-brand fw-bold">
+            TITAN <span className="text-info">EYE+</span>
+          </a>
+
+          <button
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarMenu"
           >
-            Eyeglasses ▾
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            {showEyeglasses && (
-              <div className="mega-menu">
-                <div className="mega-content">
-                  <div className="mega-column">
-                    <h4>GENDER</h4>
-                    <p>All</p>
-                    <p>Men</p>
-                    <p>Women</p>
-                    <p>Kids</p>
+          <div className="collapse navbar-collapse" id="navbarMenu">
 
-                    <h4>COLLECTION</h4>
-                    <p>EyeX</p>
-                    <p>Tees</p>
-                    <p>Signature</p>
-                    <p>Spiderman</p>
-                    <p>Hipster</p>
-                    <p>Emily in Paris</p>
+            <ul className="navbar-nav mx-auto gap-4">
+
+              {/* Eyeglasses Mega Menu */}
+              <li className="nav-item dropdown hover-dropdown">
+
+                <span className="nav-link">Eyeglasses ▾</span>
+
+               <div className="dropdown-menu mega-menu p-4">
+
+                <div className="row">
+
+                  <div className="col-12 col-md-3">
+                    <h6>GENDER</h6>
+                    <a href="#">All</a>
+                    <a href="#">Men</a>
+                    <a href="#">Women</a>
+                    <a href="#">Kids</a>
                   </div>
 
-                  <div className="mega-column">
-                    <h4>SHAPE</h4>
-                    <p>Rectangle</p>
-                    <p>Round</p>
-                    <p>Cat Eye</p>
-                    <p>Geometric</p>
-                    <p>Wayfarer</p>
-
-                    <h4>STYLE</h4>
-                    <p>Rimmed</p>
-                    <p>Semi-Rimmed</p>
-                    <p>Rimless</p>
+                  <div className="col-12 col-md-3">
+                    <h6>SHAPE</h6>
+                    <a href="#">Rectangle</a>
+                    <a href="#">Round</a>
+                    <a href="#">Cat Eye</a>
+                    <a href="#">Wayfarer</a>
                   </div>
 
-                  <div className="mega-column">
-                    <h4>TOP BRANDS</h4>
-                    <p>Zefr</p>
-                    <p>Titan</p>
-                    <p>Fastrack</p>
-                    <p>Rayban</p>
-                    <p>Dash For Kids</p>
-                    <p>Aristo</p>
-                    <p>Vogue Eyewear</p>
-                    <p>Tommy Hilfiger</p>
-                    <p>Emporio Armani</p>
-                    <p>ZEISS</p>
+                  <div className="col-12 col-md-3">
+                    <h6>TOP BRANDS</h6>
+                    <a href="#">Titan</a>
+                    <a href="#">Rayban</a>
+                    <a href="#">Fastrack</a>
+                    <a href="#">Vogue</a>
                   </div>
 
-                  <div className="mega-column">
-                    <h4>FEATURED BRANDS</h4>
-                    <p>Oakley</p>
-                    <p>Carrera</p>
-                    <p>Stepper</p>
-                    <p>Burberry</p>
-                    <p>Tom Ford</p>
-                    <p>Ted Baker</p>
-                    <p>Silhouette</p>
-                    <p>Swarovski</p>
-                    <p>Mont Blanc</p>
-                    <p>Calvin Klein</p>
-                  </div>
-
-                  <div className="mega-image">
+                  <div className="col-12 col-md-3 text-center">
                     <img
                       src="https://images.unsplash.com/photo-1574258495973-f010dfbb5371"
+                      className="img-fluid rounded"
                       alt="Eyeglasses"
                     />
-                    <span className="image-text">Eyeglasses</span>
                   </div>
+
                 </div>
+
               </div>
-            )}
-          </li>
+              </li>
 
-          {/* Sunglasses */}
-          <li
-            className="mega-parent"
-            onMouseEnter={() => setShowMegaMenu(true)}
-            onMouseLeave={() => setShowMegaMenu(false)}
-          >
-            Sunglasses ▾
+              <li className="nav-item  dropdown hover-dropdown">
+                <span className="nav-link">Sunglasses ▾</span>
+                <div className="dropdown-menu mega-menu p-4">
 
-            {showMegaMenu && (
-              <div className="mega-menu">
-                <div className="mega-content">
-                  <div className="mega-column">
-                    <h4>GENDER</h4>
-                    <p>All</p>
-                    <p>Men</p>
-                    <p>Women</p>
-                    <p>Kids</p>
+                <div className="row">
 
-                    <h4>STYLE</h4>
-                    <p>Mirrored</p>
-                    <p>Tinted</p>
-                    <p>UV Protection</p>
-                    <p>Polarized</p>
+                  <div className="col-12 col-md-3">
+                    <h6>GENDER</h6>
+                    <a href="#">All</a>
+                    <a href="#">Men</a>
+                    <a href="#">Women</a>
+                    <a href="#">Kids</a>
 
-                    <h4>USAGE</h4>
-                    <p>Regular</p>
-                    <p>Power</p>
+                    <h6 className="mt-3">STYLE</h6>
+                    <a href="#">Mirrored</a>
+                    <a href="#">Tinted</a>
+                    <a href="#">UV Protection</a>
+                    <a href="#">Polarized</a>
+
+                    <h6 className="mt-3">USAGE</h6>
+                    <a href="#">Regular</a>
+                    <a href="#">Power</a>
                   </div>
 
-                  <div className="mega-column">
-                    <h4>COLLECTION</h4>
-                    <p>Smart Sunglasses</p>
-                    <p>Donald</p>
-                    <p>Glow Up</p>
-                    <p>Whiplash</p>
-                    <p>Vivid Geometry</p>
+                  <div className="col-12 col-md-3">
+                    <h6>COLLECTION</h6>
+                    <a href="#">Smart Sunglasses</a>
+                    <a href="#">Donald</a>
+                    <a href="#">Glow Up</a>
+                    <a href="#">Whiplash</a>
+                    <a href="#">Vivid Geometry</a>
 
-                    <h4>SHAPE</h4>
-                    <p>Aviator</p>
-                    <p>Wayfarer</p>
-                    <p>Wraparound</p>
-                    <p>Rectangle</p>
-                    <p>Round</p>
+                    <h6 className="mt-3">SHAPE</h6>
+                    <a href="#">Aviator</a>
+                    <a href="#">Wayfarer</a>
+                    <a href="#">Wraparound</a>
+                    <a href="#">Rectangle</a>
+                    <a href="#">Round</a>
                   </div>
 
-                  <div className="mega-column">
-                    <h4>BRANDS</h4>
-                    <p>Titan</p>
-                    <p>Fastrack</p>
-                    <p>Rayban</p>
-                    <p>Oakley</p>
-                    <p>Burberry</p>
+                  <div className="col-12 col-md-3">
+                    <h6>BRANDS</h6>
+                    <a href="#">Titan</a>
+                    <a href="#">Fastrack</a>
+                    <a href="#">Rayban</a>
+                    <a href="#">Oakley</a>
+                    <a href="#">Burberry</a>
                   </div>
 
-                  <div className="mega-image">
+                  <div className="col-12 col-md-3 text-center">
                     <img
                       src="https://images.unsplash.com/photo-1511499767150-a48a237f0083"
+                      className="img-fluid rounded"
                       alt="Sunglasses"
                     />
-                    <span className="image-text">Sunglasses</span>
                   </div>
+
                 </div>
+
               </div>
-            )}
-          </li>
+              </li>
 
-          <li>Power Sunglasses</li>
-          <li>Computer Glasses</li>
-          <li>Hearing Aids</li>
+              <li className="nav-item">
+                <span className="nav-link">Power Sunglasses</span>
+              </li>
 
-          {/* Contact */}
-          <li
-            className="nav-item"
-            onMouseEnter={() => setShowContact(true)}
-            onMouseLeave={() => setShowContact(false)}
-          >
-            Contact Lenses ▾
+              <li className="nav-item">
+                <span className="nav-link">Computer Glasses</span>
+              </li>
 
-            {showContact && (
-              <div className="dropdown">
-                <ul>
-                  <li>EK TARA TEST</li>
-                  <li>BAUSCH & LOMB</li>
-                  <li>JOHNSON & JOHNSON</li>
-                  <li>COOPER VISION</li>
+              <li className="nav-item">
+                <span className="nav-link">Hearing Aids</span>
+              </li>
+
+              {/* Contact Lenses */}
+              <li className="nav-item dropdown hover-dropdown">
+
+                <span className="nav-link">Contact Lenses ▾</span>
+
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Bausch & Lomb</a></li>
+                  <li><a className="dropdown-item" href="#">Johnson & Johnson</a></li>
+                  <li><a className="dropdown-item" href="#">Cooper Vision</a></li>
                 </ul>
-              </div>
-            )}
-          </li>
 
-          {/* Accessories */}
-          <li
-            className="nav-item"
-            onMouseEnter={() => setShowAccessories(true)}
-            onMouseLeave={() => setShowAccessories(false)}
-          >
-            Accessories ▾
+              </li>
 
-            {showAccessories && (
-              <div className="dropdown">
-                <ul>
-                  <li>EK TARA TEST</li>
-                  <li>SWIMMING GOGGLES</li>
-                  <li>CONTACT LENS SOLUTION</li>
-                  <li>WIPES</li>
-                  <li>FACE MASKS</li>
-                  <li>BINOCULARS</li>
-                  <li>PACKAGING CASE</li>
-                  <li>CHAINS</li>
+              {/* Accessories */}
+              <li className="nav-item dropdown hover-dropdown">
+
+                <span className="nav-link">Accessories ▾</span>
+
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Cleaning Cloth</a></li>
+                  <li><a className="dropdown-item" href="#">Lens Spray</a></li>
+                  <li><a className="dropdown-item" href="#">Cases</a></li>
                 </ul>
-              </div>
-            )}
-          </li>
 
-          {/* Brands */}
-          <li
-            className="nav-item"
-            onMouseEnter={() => setShowBrands(true)}
-            onMouseLeave={() => setShowBrands(false)}
-          >
-            Brands ▾
+              </li>
 
-            {showBrands && (
-              <div className="dropdown">
-                <ul>
-                  <li>TITAN</li>
-                  <li>FASTRACK</li>
-                  <li>ZEFR</li>
-                  <li>VOGUE EYEWEAR</li>
-                  <li>RAY BAN</li>
-                  <li>MAUI JIM</li>
-                  <li>TED-BAKER</li>
-                  <li>EMPORIO ARMANI</li>
-                  <li>TEES BY FASTRACK</li>
-                  <li>ACUVUE BY J&J</li>
-                  <li>BAUSCH AND LOMB</li>
-                  <li>ZEISS</li>
-                  <li>ARISTO</li>
-                </ul>
-              </div>
-            )}
-          </li>
-        </ul>
+            </ul>
 
-        {/* Cart */}
-        <div className="cart">
-          <ShoppingCart size={22} />
+            <div>
+              <ShoppingCart size={22} />
+            </div>
+
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 };
