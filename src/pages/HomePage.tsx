079718df1399c61ee115c3e3b863a234b5fd13dd
Henry Carter <shinex2developer@gmail.com>
2026-03-06 11:32:20 +0530

    made the go to top page component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -12,6 +12,7 @@ import FeaturedCollectionsOnlyFocus from "../components/ui/FeautredCollection/Fe
 import TrendingCarousel from "../components/ui/TrendingCarousel/TrendingCarousel";
 import TestimonialsCarousel from "../components/ui/TestimonialsCarousel/TestimonialsCarousel";
 import ProductCarousel from "../components/ui/ProductsCarousel/ProductCarousel";
+import GoToTop from "../components/ui/GoToTop/GoToTop";
 
 
 const HomePage = () => {
@@ -31,8 +32,8 @@ const HomePage = () => {
       <TrendingCarousel trendingSection={data.trendingSection} />
       <TestimonialsCarousel testimonialsSection={data.testimonialsSection} />
       <FaqSection faqData={data.faq} />
-
-      <ProductCarousel
+      <GoToTop />
+      {/* <ProductCarousel
         title={data.bestsellersSection.title}
         highlight={data.bestsellersSection.highlight}
         cards={data.bestsellersSection.cards}
@@ -43,7 +44,7 @@ const HomePage = () => {
         highlight="Loved"
         filters={data.trendingSection.filters}
         filteredCards={data.trendingSection.cards}
-      />
+      /> */}
     </div>
   );
 };
